2559450d144d237f91146e553fea245ad6a64944
EdouardW <edouardwambergue@gmail.com>
2021-02-11 16:46:55 +0100

    MAj strcuture

--- whoscored.py
+++ whoscored.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
 
-from selenium import webdriver 
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-from Team import Team
+from data_model import Team
 import time
 import sys
 import os
 
+
 def initDriver():
-  driver = webdriver.Chrome(ChromeDriverManager().install())
-  return driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    return driver
 
-#standings-18594
+
 def getAllTeamsLigue1():
-  teams = []
-  driver = initDriver()
-  driver.get("https://www.whoscored.com/Regions/74/Tournaments/22/France-Ligue-1")
-  time.sleep(2) 
-  teamsList = driver.find_elements_by_xpath("//*[@id='standings-18594-content']/tr[*]/td[*]/a[@class='team-link ']")
-  
-  i = 0
-  while i < len(teamsList):
-    anchor = teamsList[i]
-    teamURL = anchor.get_attribute("href")
-    teamName = anchor.get_attribute("innerHTML")
-    teamURL = teamURL.replace("/Show/","/Fixtures/")
-    teams.append(Team(teamName, teamURL))
-    
-    i += 1
-  driver.close()
-  
-  return teams
+    # standings-18594
+    teams = []
+    driver = initDriver()
+    driver.get("https://www.whoscored.com/Regions/74/Tournaments/22/France-Ligue-1")
+    time.sleep(2)
+    teamsList = driver.find_elements_by_xpath("//*[@id='standings-18594-content']/tr[*]/td[*]/a[@class='team-link ']")
 
+    i = 0
+    while i < len(teamsList):
+        anchor = teamsList[i]
+        teamURL = anchor.get_attribute("href")
+        teamName = anchor.get_attribute("innerHTML")
+        teamURL = teamURL.replace("/Show/","/Fixtures/")
+        teams.append(Team(teamName, teamURL))
+
+        i += 1
+    driver.close()
+
+    return teams
 
 
 def menu():
