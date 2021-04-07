1fb45d0a21793873d0df840f3b5c464233b1f889
EdouardW <edouardwambergue@gmail.com>
2021-04-07 18:29:47 +0200

    MAJ

--- ML/logistic_regression.py
+++ ML/logistic_regression.py
@@ -5,7 +5,7 @@ from sklearn import linear_model
 
 df = load_dataframe('betai/ML/input/FRANCE_ligue1.csv')
 df = delete_first_day(df)
-
+print(df.head())
 categorical_features = ['FTR', 'HTR']
 df = handle_categorical(df, categorical_features)
 
@@ -20,10 +20,7 @@ features_to_keep = ['journee','cl_hometeam','cl_awayteam', 'points_h', 'gagnes_h
 #df = select_features(df, features_to_keep)
 
 
-x = df[['journee','cl_hometeam','cl_awayteam', 'points_h', 'gagnes_h', 'nuls_h',\
-    'perdus_h', 'buts_h', 'contre_h', 'points_a', 'gagnes_a', 'nuls_a', 'perdus_a', \
-        'buts_a', 'contre_a', 'forme_h_win', 'forme_h_draw', 'forme_h_lose', \
-            'forme_a_win', 'forme_a_draw', 'forme_a_lose', 'B365H', 'B365D', 'B365A']]
+x = df[['cl_hometeam','cl_awayteam']]
 y = df['FTR']
 
 
@@ -33,6 +30,6 @@ modele_regLog = linear_model.LogisticRegression(random_state = 0, solver = 'sag'
 modele_regLog.fit(x_train, y_train)
 
 precision = modele_regLog.score(x_test, y_test)
-print(precision*100)
+#print(precision*100)
 
 #'https://www.cours-gratuit.com/tutoriel-python/tutoriel-python-matriser-la-rgression-logistique-avec-scikit-learn'
